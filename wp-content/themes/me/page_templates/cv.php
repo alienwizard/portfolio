976c650989b17a82e552033e6697fad3a21f5b82
johan wendin <johan wendin>
2016-10-28 07:25:21 +0200

    working on cv page

--- wp-content/themes/me/page_templates/cv.php
+++ wp-content/themes/me/page_templates/cv.php
@@ -15,9 +15,7 @@
 <p class="info">Web developer based in Stockholm with a passion for user interaction and user experience design</p>
 <div class="top-section">
 <div class="me">
-
 </div>
-
 </div>
 <div id="past-experiences">
 	<h2>Past Experiences</h2>
@@ -67,36 +65,60 @@
 		<li><h3>Photoshop: </h3></li>
 		<li><h3>Basic Knowledge of ADobe Premier. Used in project with friends and in some courses at upper secondary school</h3></li>
 		<li>
-				<h3>Programming languages</h3>
+				<h3>Web Development</h3>
 				<p>PHP mostly in a Wordpress environment</p>
 				<p>Very experienced with Javascript, mostly with the Jquery library and React</p>
-				<p>basic knowledge of Java from a few courses at school and some small projects at home</p>
-				<p>Knowlege of the C++ programming language</p>
-
+        <p>The HMTL markup language is like the second alphabet of my life. Experience of using a variety of HTML 5 tags and attributes. I have experience building my HTML with Jade.</p>
+        <p>CSS and CCS3 usually compiled from a SASS. I have worked with a variety of psuedo elements and other CSS3 features. I always write sites with responsive designs using the CSS3 Media querys</p>
+        <p>Currently building all my projects with SASS. Webpack compiles all my SASS into minified css for the browser.</p>
+        <p>Very experienced with JQuery. All my projects have Jquery in the front-end</p>
+        <p>MySQL with Wordpress and also with </p>
+        <p>Experienced with Node.js</p>
+        <p>Working knowledge of React. This is currently my favorite technology to work with on my spare time</p>
+        <p>Angular.js is nothing i have been using at work, but i think its a exciting technology and i have been doing a few projects at home and getting to know the technology a bit more</p>
+        <p>
+        <a href="https://github.com/alienwizard">Git</a>
+        <p></p>
+        <p>Grunt is something that i used alot before as my task runner, but i've recently started using Webpack more and more</p>
+        <p>Webpack</p>
 		</li>
 		<li>
 				<h3>Other</h3>
-				<p>The HMTL markup language is like the second alphabet of my life. Experience of using a variety of HTML 5 tags and attributes. I have experience building my HTML with Jade.</p>
-				<p>CSS and CCS3 usually compiled from a SASS. I have worked with a variety of psuedo elements and other CSS3 features. I always write sites with responsive designs using the CSS3 Media querys</p>
-				<p>Currently building all my projects with SASS. Webpack compiles all my SASS into minified css for the browser.</p>
-        <p>Very experienced with JQuery. All my projects have Jquery in the front-end</p>
-				<p>MySQL with Wordpress and also with </p>
-				<p>Experienced with Node.js</p>
-				<p>Working knowledge of React. This is currently my favorite technology to work with on my spare time</p>
-				<p>Angular.js is nothing i have been using at work, but i think its a exciting technology and i have been doing a few projects at home and getting to know the technology a bit more</p>
-        <p>
-				<a href="https://github.com/alienwizard">Git</a>
-        <p></p>
-				<p>Grunt is something that i used alot before as my task runner, but i've recently started using Webpack more and more</p>
-        <p>Webpack</p>
+        <p>basic knowledge of Java from a few courses at school and some small projects at home</p>
+        <p>Knowlege of the C++ programming language</p>
 		</li>
 
 	</ul>
   <h2>Project</h2>
   <ul>
     <li>
-      <h3>Other</h3>
-      <p>Arbetet varade mellan maj/juni någon gång till slutet av augusti. Kunden hade själv marknadsfört hemsidan på facebook tidigare och sett resultat från detta och kände nu att de behövde en lite uppfräschning av hemsidan för att kunna driva trafiken till något som ska kunna generera leads! En viktig punk för sidan var att driva trafiken till instagram och få folk kopplade till verksamheten, så att de sedan kan nås enkelt av marknadsföringen. Det ska även vara enkelt att nå säljarna via hemsidan. Den första kontakten med säljarna ska inte vara mer än några klick bort vart du än befinner dig på sidan.</p></li>
+      <h3>Traktor och Maskin i Sörmland AB</h3>
+      <p>Arbetet varade mellan maj/juni någon gång till slutet av augusti.
+         Kunden hade själv marknadsfört hemsidan på facebook tidigare och sett resultat från detta och kände nu att de behövde en lite uppfräschning av hemsidan för att kunna driva trafiken till något som ska kunna generera leads!
+         En viktig punk för sidan var att driva trafiken till instagram och få folk kopplade till verksamheten,
+         så att de sedan kan nås enkelt av marknadsföringen. Det ska även vara enkelt att nå säljarna via hemsidan.
+          Den första kontakten med säljarna ska inte vara mer än några klick bort vart du än befinner dig på sidan.</p>
+      </li>
+      <li>
+        <h3>Landqvist Handelsträdgård</h3>
+        <p>Kunden var väldigt intresserad och hade utvecklat den tidigare hemsidan själv.
+           Detta utvecklade en viss kravbild på att sidan skulle vara anpassningsbar.
+            Det fanns även krav på att de skulle ha en instagram-feed på sidan i någon form,
+          detta eftersom de är väldigt aktiva på den socialaplattformen och tar många bilder på trädgården dagligen.
+          Arbetsprocessen var sådan att kunden berättade vilken funktionalitet de ville ha på sidan och hade inte riktigt några krav på sidans design,
+          utan litade på att jag skulle ta fram några förslag. Efter lite bollande så kom vi fram till något som alla gillade.
+          En informativ hemsida som inte är för krånglig. Eftersom att deras verksamhet är väldigt säsongsberoende så var de viktigt med de olika årstidernas öppettider högt upp på sidan,
+          så att användarna ser det direkt när de kommer in på sidan.
+          Nästa steg var att programmera sidan med Wordpress. Kunden ville ha sidan väldigt anpassningsbar och användarvänlig, det går alltså att byta färg och m.m genom wp-options-API. För instagram har jag använt instafeed.js. Kände att jag själv inte behövde gräva i API't för min lösning!</p>
+        </li>
+        <li>
+          <h3>Trepe Bilvård</h3>
+          <p>I detta projekt fick jag en lektion i branding och hur det är att förhålla sig till ett varumärke!
+            Trepe bilvård är en del av Autoexperten och jag fick tillgång till deras intranet och alla guider och regler man måste förhålla sig till när man utvecklar något med deras varumärke.
+            Kunden ville också att sidan skulle vara lik Moderhemsidan https://www.autoexperten.se/. här har jag även behövt bygga ut wordpress en del med custom posts och olika dynamiska fält.
+            Det finns också möjligheten för kunder att "logga in" och begära en tid att komma till verkstan.
+            Detta görs mot wordpress egna databas api och informationen kunderna fyller i sparas där och sedan kommer sidans administratörer åt  kunderna från wp-admin</p>
+          </li>
   </ul>
 </div>
 </div>
